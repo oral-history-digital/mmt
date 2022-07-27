3d94935091da3e761526e67216dbbb48a2005c0b
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2022-07-27 18:13:56 +0200

    Install passport

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const passport = require('passport');
 
 const app = express();
 const port = 3000;
 const host = 'localhost';
 
 app.get('/', (req, res) => {
+    passport.authenticate('local');
+
+    console.log('authenticated');
+
     res.send('hello world');
 });
 
