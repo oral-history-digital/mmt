64f1ce0b2fb730c821532fb024e18d0bbcdf1e15
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2022-08-08 16:30:13 +0200

    Finish progress bar

--- public/app.js
+++ public/app.js
@@ -39,6 +39,15 @@ function handleFileChange(event) {
     console.log(uploadObject);
     uploadObject.addEventListener('progress', (event) => {
         console.log(event.lengthComputable, event.loaded, event.total);
+
+        if (event.lengthComputable) {
+            const percentage = Math.round(100 / event.total * event.loaded);
+
+            const progressBar = document.getElementById('progress-bar');
+
+            progressBar.value = percentage;
+            progressBar.innerHTML = `${percentage}%`;
+        }
     });
 
     uploadObject.addEventListener('load', (event) => {
@@ -46,9 +55,43 @@ function handleFileChange(event) {
 
         fileInput.value = null;
         fileInput.disabled = false;
+
+        removeProgressBar(firstFile.name);
     });
+
+    addProgressBar(firstFile.name);
 
     req.send(formData);
 
     console.log(req);
+}
+
+function addProgressBar(filename) {
+    const container = document.getElementById('progress-bar-container');
+
+    const message = document.createElement('p');
+    message.id = 'progress-bar-message';
+    message.className = 'mt-5';
+    message.innerHTML = `Uploading file <b>${filename}…</b>`;
+
+
+    const progressBar = document.createElement('progress');
+
+    console.log(progressBar);
+
+    progressBar.id = 'progress-bar';
+    progressBar.className = 'progress is-primary mt-3';
+    progressBar.max = 100;
+    progressBar.value = 0;
+    progressBar.innerHTML = '0%';
+
+    container.append(message);
+    container.append(progressBar);
+}
+
+function removeProgressBar(filename) {
+    const progressBar = document.getElementById('progress-bar');
+    progressBar.remove();
+    const message = document.getElementById('progress-bar-message');
+    message.innerHTML = `File <b>${filename}</b> uploaded. <a href="/files">See files.</a>`;
 }
