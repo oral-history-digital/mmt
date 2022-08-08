81aced435c13ff89de652777fd02a3c87991ed39
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2022-08-08 18:30:47 +0200

    Add some production stuff

--- server.js
+++ server.js
@@ -3,17 +3,21 @@ const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const compression = require('compression');
+const helmet = require('helmet');
 
 const injectUser = require('./injectUser');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(helmet());
 app.use(express.static('public', { maxAge: '1m' }));
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(injectUser);
+app.use(compression());
 
 app.engine('hbs', exphbs.engine({extname: '.hbs'}));
 
@@ -27,5 +31,3 @@ const host = 'localhost';
 app.listen(port, host, () => {
     console.log(`App listening on http://${host}:${port}`);
 });
-
-export const viteNodeApp = app;
