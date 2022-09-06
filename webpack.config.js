d97ebe2b7872ea3bfbae58e79946c4945cb4a35e
Marc Altmann <marcaltmann@posteo.de>
2022-09-06 09:34:24 +0200

    Try out new JS style

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,46 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
-    entry: path.resolve(__dirname, './client/src/index.js'),
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
+  entry: path.resolve(__dirname, './client/src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'build')
     },
-    resolve: {
-        extensions: ['*', '.js', '.jsx'],
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'build'),
-        },
-        port: 4000,
-        proxy: {
-            '/api': 'http://localhost:3000',
-        },
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'client', 'src', 'index.html'),
-        }),
-        new FaviconsWebpackPlugin({
-            logo: path.join(__dirname, 'client', 'src', 'ohd-logo.png'),
-            mode: 'light',
-        }),
-    ],
-};
+    port: 4000,
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'client', 'src', 'index.html')
+    }),
+    new FaviconsWebpackPlugin({
+      logo: path.join(__dirname, 'client', 'src', 'ohd-logo.png'),
+      mode: 'light'
+    })
+  ]
+}
