d5ddb9e579bede89d3194cac47ca6e95d2684ca0
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2022-09-15 11:35:56 +0200

    Work in progress language option

--- client/modules/auth/Profile.jsx
+++ client/modules/auth/Profile.jsx
@@ -1,37 +1,68 @@
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import useSWR, { useSWRConfig } from 'swr';
 
 import RequireAuth from './RequireAuth';
 import { getUser } from './selectors';
 
 export default function Profile() {
-    const user = useSelector(getUser);
-    const { t } = useTranslation();
+  const user = useSelector(getUser);
+  const { t, i18n } = useTranslation();
+  const { mutate } = useSWRConfig();
 
-    return (
-        <RequireAuth>
-            <section className="section">
-                <div className="container">
-                        <h1 className="title is-spaced">
-                            {t('modules.auth.profile.title')}
-                        </h1>
+  function handleLanguageChange(event) {
+    const lang = event.target.value;
 
-                        {user && (
-                            <dl>
-                                <dt><b>{t('modules.auth.profile.username')}</b></dt>
-                                <dd>{user.username}</dd>
+    i18n.changeLanguage(lang);
+  }
 
-                                <dt><b>{t('modules.auth.profile.email')}</b></dt>
-                                <dd>{user.email}</dd>
+  return (
+    <RequireAuth>
+      <section className="section">
+        <div className="container">
+          <h1 className="title is-spaced">
+            {t('modules.auth.profile.title')}
+          </h1>
 
-                                <dt><b>{t('modules.auth.profile.language')}</b></dt>
-                                <dd>
-                                    {t(`global.languages.${user.language}`)}
-                                </dd>
-                            </dl>
-                        )}
+          {user && (
+            <dl>
+              <dt><b>{t('modules.auth.profile.username')}</b></dt>
+              <dd>{user.username}</dd>
+
+              <dt><b>{t('modules.auth.profile.email')}</b></dt>
+              <dd>{user.email}</dd>
+
+              <dt><b>{t('modules.auth.profile.language')}</b></dt>
+              <dd>
+                <div className="control">
+                  <label className="radio">
+                    <input
+                      type="radio"
+                      name="language"
+                      value="en"
+                      checked={i18n.language === 'en'}
+                      onChange={handleLanguageChange}
+                    />
+                      {' '}
+                      {t('global.languages.en')}
+                  </label>
+                  <label className="radio">
+                    <input
+                      type="radio"
+                      name="language"
+                      value="de"
+                      checked={i18n.language === 'de'}
+                      onChange={handleLanguageChange}
+                    />
+                      {' '}
+                      {t('global.languages.de')}
+                  </label>
                 </div>
-            </section>
-        </RequireAuth>
-    );
+              </dd>
+            </dl>
+          )}
+        </div>
+      </section>
+    </RequireAuth>
+  );
 }
