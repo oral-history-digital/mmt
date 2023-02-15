bba9039042422c406fee1e3cab8a40b6e638fbc4
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2023-02-15 11:38:16 +0100

    add getFilenameSuffix function

---
+++ server/utilities/getFilenameSuffix.js
@@ -0,0 +1,11 @@
+module.exports = function getFilenameSuffix(date) {
+  const year = date.getFullYear().toString();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+
+  const suffix = `${year}${month}${day}${hours}${minutes}${seconds}`;
+  return suffix;
+};
