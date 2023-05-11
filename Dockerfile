80c6153c076eb1779c0a3f51c2434ad9ac9b63d8
Marc Altmann <marc.altmann@cedis.fu-berlin.de>
2023-05-11 08:17:15 +0200

    Remove Webpack

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,16 @@
-FROM docker.io/library/node:18-bullseye-slim
+FROM node:18-bullseye-slim
 
-#VOLUME user_files
 WORKDIR /app
 
-COPY package.json package-lock.json ./
-
-ENV NODE_ENV=production
-RUN npm ci && npm cache clean --force
+COPY package*.json ./
+RUN npm ci --only=production && npm cache clean --force
 
 COPY . .
-RUN npm run build
+RUN npm run build && mv ./dist/* ./public/ && rm -rf ./dist
 
-ENV MMT_LISTEN_HOST=0.0.0.0
-ENV MMT_LISTEN_PORT=3000
-ENV MMT_USER_FILES_DIR=/app/user_files
-CMD ["node", "server/server.js"]
+ENV MMT_LISTEN_HOST=0.0.0.0 \
+    MMT_LISTEN_PORT=3000 \
+    MMT_USER_FILES_DIR=/app/user_files
+
 EXPOSE 3000
+CMD ["node", "server/server.js"]
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+
+console.log(path.resolve(__dirname, 'client/modules'))
 
 export default defineConfig({
+  plugins: [
+    // …
+    react({
+      // Use React plugin in all *.jsx and *.tsx files
+      include: '**/*.{jsx,tsx}',
+    }),
+  ],
   root: 'client',
   build: {
     // Relative to the root
@@ -8,5 +19,21 @@ export default defineConfig({
   },
   define: {
     '__VERSION__': JSON.stringify(process.env.npm_package_version),
-  }
+  },
+  server: {
+    port: 4000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        //secure: false,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'client'),
+      modules: path.resolve(__dirname, 'client/modules'),
+    },
+  },
 });
